562c6e86a92462558238d9c60e9226259baa29e6
nd2215 <nd2215@ic.ac.uk>
2017-10-25 12:09:45 +0000

    Left 90 calibrated.

--- turn_definitions.py
+++ turn_definitions.py
@@ -26,43 +26,23 @@ motorParamsLeft = motorParams
 interface.setMotorAngleControllerParameters(motors[0],motorParamsLeft)
 interface.setMotorAngleControllerParameters(motors[1],motorParamsRight)
 
+THRESHOLD = 1.05
+
 def Left90deg():
-#    while not interface.motorAngleReferencesReached(motors) :
- #       motorAngles = interface.getMotorAngles(motors)
-  #          if motorAngles :
-                
-   #         time.sleep(0.1)
-    speed = motorParams.maxRotationAcceleration
-    angle = 3.78839
-
+    print("turning 90 left")
+    angle = 6.1
+    startTime = time.time()
     interface.increaseMotorAngleReferences(motors, [angle, -angle])
-
-#    interface.setMotorAngleControllerParameters(motors[0],motorParams)
- #   interface.setMotorAngleControllerParameters(motors[1],motorParams)
-
-    interface.setMotorRotationSpeedReferences(motors,[speed,-speed])
-
+    while interface.motorRotationSpeedReferenceReached:
+        timeNow = time.time()
+	if timeNow - startTime > THRESHOLD:
+		print "stopping because we did not reach"
+		break
     
 def Right90deg():
     pass
 
-#while True:
-#	angle = float(input("Enter a angle to rotate (in radians): "))
 
-
-#	interface.increaseMotorAngleReferences(motors,[angle,angle])
-
-#	while not interface.motorAngleReferencesReached(motors) :
-#		motorAngles = interface.getMotorAngles(motors)
-#		if motorAngles :
-#			print "Motor angles: ", motorAngles[0][0], ", ", motorAngles[1][0]
-#		time.sleep(0.1)
-
-#	print "Destination reached!
-
-
-while True:
-    Left90deg()
-    time.sleep(10000)
+Left90deg()
 
 interface.terminate()
